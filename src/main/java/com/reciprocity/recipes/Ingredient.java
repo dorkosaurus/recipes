158788b0fcc99294402e64c926d58b6b2e9396aa
Vivek Ramaswamy <ramaswamy.vivek@gene.com>
2015-07-26 22:21:02 -0700

    ingredient and recipe

---
+++ src/main/java/com/reciprocity/recipes/Ingredient.java
@@ -0,0 +1,9 @@
+package com.reciprocity.recipes;
+
+/**
+    An ingredient is an 'identity' level item that is used in a recipe.
+*/
+public class Ingredient{
+    
+    
+}
---
+++ src/main/java/com/reciprocity/recipes/Recipe.java
@@ -0,0 +1,9 @@
+package com.reciprocity.recipes;
+
+/**
+    A recipe is a combination of 'parental' reagents and the instructions
+*/
+public class Recipe{
+    
+    
+}
